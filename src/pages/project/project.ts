a419d2b6200a4c67459337aa3d6d3e2a12ee9011
lyx <584719733@qq.com>
2018-01-03 23:17:51 +0800

    列表页除了筛选，实在太难受了，明天再弄吧

--- src/pages/project/project.ts
+++ src/pages/project/project.ts
@@ -8,45 +8,24 @@ import { projectViewPage } from './view/project_view';
   templateUrl: 'project.html'
 })
 export class ProjectPage {
-  project: string;
-  list:number = 1;
-  projects: object = {
-    thumb: "assets/image/pront_03.png",
-    name: "海信金融PPP項目",
-    date: "17-05",
-    content: "在等待的日子里刻苦读书锻炼身体谦卑做人............"
-  }
-  items = [];
+  genders:string;
+  public optionarr:any = [
+    "本库","XX分库","XX分库","XX分库","XX分库","XX分库","XX分库","XX分库","XX分库"
+  ]
   constructor(public navCtrl: NavController) {
-    this.project = "pc"
-    for (let i = 0; i < 5; i++) {
-      this.items.push(this.projects);
-    }
-  }
-  listPage() {
-    this.list+=1
-    this.navCtrl.push(projectViewPage,{
-      list:this.list
-    })
   }
   // 下拉刷新
   doRefresh(refresher) {
-    console.log('Begin async operation', refresher.currentY);
-
     setTimeout(() => {
-      console.log('Async operation has ended');
       refresher.complete();
     }, 2000);
   }
   // 上啦加载
   doInfinite(infiniteScroll) {
     {
-      console.log('Begin async operation');
+ 
 
       setTimeout(() => {
-        for (let i = 0; i < 5; i++) {
-          this.items.push(this.projects);
-        }
         infiniteScroll.complete();
       }, 500);
     }
