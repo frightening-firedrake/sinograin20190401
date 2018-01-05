eaf7683cabd5ef30750f917863bbda6372ed49d6
lyx <584719733@qq.com>
2018-01-05 09:35:17 +0800

    登录完善

--- src/app/routerConfig.ts
+++ src/app/routerConfig.ts
@@ -2,12 +2,14 @@ import { DeepLinkConfig } from 'ionic-angular';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
+import { loginPage } from '../pages/login/login'
 import { ProjectPage } from '../pages/project/project';
 import { SearchPage } from '../pages/home/search/search';
 
 export const linkConfig: DeepLinkConfig = {
   links: [
-    // { component: TabsPage, name: 'Tabs', segment: 'tab' },
+    { component: loginPage, name: 'login' ,segment: 'login' },
+    { component: TabsPage, name: 'Tabs', segment: 'tab' },
     // { component: HomePage, name: 'Home', segment: 'home' },
     // { component: TeamsPage, name: 'Teams', segment: 'teams' },
     // { component: TechPage, name: 'Tech', segment: 'tech' },
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,10 @@
 import { Component } from "@angular/core";
+import { NavController,ViewController  } from "ionic-angular";
 
 import { _alertBomb } from '../common/_alert'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+import { TabsPage } from '../tabs/tabs'
 
 @Component({
     selector:"login",
@@ -9,7 +12,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 })
 
 export class loginPage{
-    constructor(public _alert:_alertBomb,  public FormBuilder: FormBuilder,){
+    constructor(public _alert:_alertBomb,public viewCtrl:ViewController, public FormBuilder: FormBuilder,public NavCtrl: NavController){
         
+    }
+    onSubmit(e){
+        // this.viewCtrl.dismiss()
+        this.NavCtrl.push(TabsPage)
     }
 }
